afc1128470a0144bd7cedff276954c96551f0f8f
khanhdocnope <khanh4346k9@gmail.com>
2026-06-10 08:26:38 +0700

    initialize database configuration module

--- js/database.js
+++ js/database.js
@@ -57,7 +57,7 @@ const MOVIE_DATABASE = [
     "banner": "https://vi.wikipedia.org/wiki/T%E1%BA%ADp_tin:Pok%C3%A9mon_The_Movie_2000.jpg",
     "rating": 7.85,
     "year": "năm 2000",
-    "duration": "85",
+    "duration": "85 phút",
     "genres": [
       "Anime",
       "Hành Động",
@@ -66,7 +66,13 @@ const MOVIE_DATABASE = [
     ],
     "description": "Pokémon Movie 2 kể về thảm họa diệt vong đe dọa trái đất khi kẻ thu thập Lawrence III bắt giữ bộ ba chim huyền thoại Articuno, Zapdos và Moltres, khiến thời tiết toàn cầu bị đảo lộn nghiêm trọng. Trở thành \"người được chọn\" theo một lời tiên tri cổ xưa tại quần đảo Orange, Satoshi phải vượt qua giông bão để thu thập ba viên ngọc nguyên tố nhằm xoa dịu thiên nhiên. Cuối cùng, nhờ sự trợ giúp dũng cảm của Pokémon hộ vệ biển cả Lugia cùng giai điệu sáo thần, Satoshi đã kịp thời hoàn thành sứ mệnh, khôi phục lại sự cân bằng cho thế giới.",
     "isFeatured": true,
-    "episodes": []
+    "episodes": [
+      {
+        "episodeId": 1,
+        "title": "Thuyết minh",
+        "videoUrl": "https://drive.google.com/file/d/1qQ1OgP5atjfxDWm-wzIY_YGcErXvBVeA/preview"
+      }
+    ]
   },
   {
     "id": "pokemon-moive-3",
@@ -76,7 +82,7 @@ const MOVIE_DATABASE = [
     "banner": "",
     "rating": 7.75,
     "year": "năm 2000",
-    "duration": "",
+    "duration": "74 phút",
     "genres": [
       "Anime",
       "Hành Động",
@@ -85,7 +91,13 @@ const MOVIE_DATABASE = [
     ],
     "description": "Bộ phim kể về Mee, một cô bé cô độc vô tình triệu hồi các Pokémon cổ xưa Unown sau khi cha cô mất tích bí ẩn. Sức mạnh tâm linh của Unown đã biến đổi toàn bộ thị trấn Greenfield xinh đẹp thành một tòa tháp tinh thể băng giá, đồng thời tạo ra Pokémon huyền thoại Entei để làm cha nuôi bảo vệ Mee. Để hoàn thiện gia đình trong mơ của cô bé, Entei đã bắt cóc mẹ của Satoshi (Ash) về tháp. Satoshi cùng các bạn buộc phải đột nhập vào thế giới ảo ảnh đầy nguy hiểm này để giải cứu mẹ, đối đầu với Entei và giúp Mee nhận ra sự thật để giải thoát cho thị trấn.",
     "isFeatured": false,
-    "episodes": []
+    "episodes": [
+      {
+        "episodeId": 1,
+        "title": "Thuyết minh",
+        "videoUrl": "https://drive.google.com/file/d/1lLx9StNRcjTRUv6mcZlbXItJWseDeHoG/preview"
+      }
+    ]
   },
   {
     "id": "pokemon-movie-4",
@@ -95,7 +107,7 @@ const MOVIE_DATABASE = [
     "banner": "",
     "rating": 7.85,
     "year": "2001",
-    "duration": "",
+    "duration": "(error)",
     "genres": [
       "Anime",
       "Hành Động",
@@ -104,7 +116,13 @@ const MOVIE_DATABASE = [
     ],
     "description": "Bộ phim bắt đầu ở quá khứ khi một cậu bé tên Yukinari (Sammy) cố gắng bảo vệ Pokémon huyền thoại Celebi khỏi tay thợ săn. Để trốn thoát, Celebi đã dùng năng lượng thức tỉnh để dịch chuyển thời gian, vô tình đưa cả hai đến thế giới hiện tại—thời điểm của Satoshi (Ash). Tại đây, Yukinari kết bạn với Satoshi nhưng nguy hiểm lại ập đến khi thợ săn bóng đêm Marauder xuất hiện. Hắn sử dụng bóng chứa bóng tối (Dark Ball) để tẩy não, biến Celebi thành một sinh vật tàn ác nhằm hủy diệt khu rừng. Để giải cứu thần rừng, Satoshi và Yukinari phải hợp lực chiến đấu cùng sự trợ giúp của Pokémon huyền thoại Suicune. Sau khi tà ác bị đánh bại và trật tự khu rừng được khôi phục, Celebi đưa Yukinari trở về thời đại của mình. Bộ phim khép lại với tiết lộ bất ngờ: cậu bé Yukinari năm xưa chính là Giáo sư Okido (Oak) lúc nhỏ.",
     "isFeatured": true,
-    "episodes": []
+    "episodes": [
+      {
+        "episodeId": 1,
+        "title": "Thuyết minh",
+        "videoUrl": "https://drive.google.com/file/d/1dwFlzItMI5kkamkHhBYjm8MOxmich7v7/preview"
+      }
+    ]
   },
   {
     "id": "pokemon-moive-5",
@@ -114,7 +132,7 @@ const MOVIE_DATABASE = [
     "banner": "",
     "rating": 8.1,
     "year": "2002",
-    "duration": "",
+    "duration": "72 phút",
     "genres": [
       "Anime",
       "Hành Động",
@@ -123,7 +141,13 @@ const MOVIE_DATABASE = [
     ],
     "description": "Bộ phim đưa Satoshi (Ash) và các bạn đến ghé thăm Alto Mare, một thành phố xinh đẹp với những con kênh uốn lượn được lấy cảm hứng từ Venice, Ý. Nơi đây được bảo hộ bởi hai anh em Pokémon huyền thoại Latios và Latias, cùng báu vật linh thiêng gọi là Giọt nước Tâm hồn",
     "isFeatured": true,
-    "episodes": []
+    "episodes": [
+      {
+        "episodeId": 1,
+        "title": "Thuyết minh",
+        "videoUrl": "https://drive.google.com/file/d/1wuBqwdzI-a7vBTsGtT-49YDNQDHWgs9c/preview"
+      }
+    ]
   },
   {
     "id": "pokemon-movie-6",
@@ -133,7 +157,7 @@ const MOVIE_DATABASE = [
     "banner": "",
     "rating": 8.7,
     "year": "năm 2003",
-    "duration": "",
+    "duration": "81 phút",
     "genres": [
       "Anime",
       "Hành Động",
@@ -142,7 +166,63 @@ const MOVIE_DATABASE = [
     ],
     "description": "Bộ phim xoay quanh Jirachi, một Pokémon huyền thoại ngàn năm mới thức tỉnh một lần trong vòng bảy ngày, trùng khớp với thời gian sao chổi Thiên Niên Kỷ đi qua trái đất. Trong dịp này, Satoshi (Ash) và các bạn đã ghé thăm một hội chợ di động và làm quen với ảo thuật gia Butler cùng trợ lý Diane. Jirachi đã thức tỉnh và chọn Masato (Max) – em trai của Haruka (May) – làm người bạn tri kỷ của mình trong những ngày ngắn ngủi này. Tuy nhiên, Butler thực chất là một cựu nhà khoa học của Đội Hỏa Tiễn (Team Rocket), kẻ âm mưu lợi dụng năng lượng sao chổi hấp thụ từ Jirachi để hồi sinh Pokémon siêu cổ đại Groudon. Thí nghiệm thất bại đã tạo ra một \"Groudon bóng tối\" giả mạo, một quái vật tàn ác hấp thụ mọi sinh vật xung quanh để tăng cường sức mạnh. Để sửa chữa sai lầm và bảo vệ các bạn, Butler đã hoàn lương, cùng Satoshi và Masato giúp Jirachi giải phóng nguồn năng lượng tối thượng, tiêu diệt quái thú và đưa Jirachi trở lại giấc ngủ ngàn năm an lành.",
     "isFeatured": true,
-    "episodes": []
+    "episodes": [
+      {
+        "episodeId": 1,
+        "title": "Thuyết minh",
+        "videoUrl": "https://drive.google.com/file/d/1XTPEWTnLivCXpP09ctut8KCvzch8zTeQ/preview"
+      }
+    ]
+  },
+  {
+    "id": "pokemon-movie-7",
+    "title": "pokemon movie 7: Deoxys kẻ xâm lăng đến từ vũ trụ.",
+    "originalTitle": "Pokémon the Movie: Destiny Deoxys.",
+    "poster": "",
+    "banner": "",
+    "rating": 8.5,
+    "year": "2004",
+    "duration": "(error)",
+    "genres": [
+      "Anime",
+      "Hành Động",
+      "Phiêu Lưu",
+      "Kỳ ảo (Fantasy)"
+    ],
+    "description": "Câu chuyện bắt đầu bằng vụ va chạm của một khối thiên thạch chứa Pokémon ngoài hành tinh Deoxys xuống Trái Đất, dẫn đến một trận chiến ác liệt với thủ lĩnh tầng ô-zôn Rayquaza. Bốn năm sau thảm họa đó, nhóm Satoshi (Ash) ghé thăm thành phố công nghệ cao LaRousse và kết bạn với Tory, một cậu bé mắc chứng sợ Pokémon nghiêm trọng do dư chấn từ sự kiện năm xưa.",
+    "isFeatured": true,
+    "episodes": [
+      {
+        "episodeId": 1,
+        "title": "Thuyết minh",
+        "videoUrl": "https://drive.google.com/file/d/1Vqar6VjpA7r0OGFHATL0AsVUNFVU-Bmz/preview"
+      }
+    ]
+  },
+  {
+    "id": "pokemon-movie-8",
+    "title": "pokemon movie 8: Mew và Người hùng làn sóng Lucario.",
+    "originalTitle": "pokemon the Movie 8: Lucario and the Mystery of Mew.",
+    "poster": "",
+    "banner": "",
+    "rating": 8.1,
+    "year": "năm 2005",
+    "duration": "101 phút",
+    "genres": [
+      "Anime",
+      "Hành Động",
+      "Phiêu Lưu",
+      "Kỳ ảo (Fantasy)"
+    ],
+    "description": "Bộ phim bắt đầu từ hàng thế kỷ trước khi vương quốc Cameran đứng trước nguy cơ diệt vong bởi một cuộc chiến tranh tàn khốc. Để bảo vệ người bạn đồng hành của mình, Hiệp sĩ Sóng tâm linh Aaron đã phong ấn Pokémon Lucario vào trong vương trượng rồi một mình đi đến Cây Khởi Nguyên để hy sinh thân mình cứu lấy thế giới.",
+    "isFeatured": true,
+    "episodes": [
+      {
+        "episodeId": 1,
+        "title": "Thuyết minh",
+        "videoUrl": "https://drive.google.com/file/d/18Sbtzxndiizh_JUOfMZeFIZCnDL_ES4m/preview"
+      }
+    ]
   }
 ];
 
